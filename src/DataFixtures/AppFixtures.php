79e6c1ea25a1f4a7553918c6ddd96af50d2bb3c8
Danijel Šugar <danijel.sugar@outlook.com>
2019-05-16 20:36:48 +0200

    Included hautelook/AliceBundle to manage fixtures and to seed database

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $loader = new NativeLoader();
+        $objectSet = $loader->loadFile(__DIR__.'/Fixtures.yml')->getObjects();
+        foreach ($objectSet as $object) {
+            $manager->persist($object);
+        }
+        $manager->flush();
+        
+    }
+}
