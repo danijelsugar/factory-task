8a60e8716a3cb286014940c3099663b953525d04
Danijel Šugar <danijel.sugar@outlook.com>
2019-05-20 21:35:45 +0200

    Working on splitting the code into several small functions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -32,20 +32,40 @@ class HomeController extends AbstractController
         IngredientMealRepository $imr,
         IngredientsRepository $ir
     ) {
-        if (isset($request->query->all()['per_page'])) {
-            $per_page = $request->query->all()['per_page'];
-        } else {
-            $per_page = 5;
-        }
 
-        if (isset($request->query->all()['lang'])) {
-            $langCode = $request->query->all()['lang'];
-            $lang = $lr->findOneBy(
-                [
-                    'code' => $langCode,
-                ]
-            );
-            if (empty($lang)) {
+        if (!empty($request->query->all())) {
+
+            if (!$this->validate($request)) {
+                return new JsonResponse(
+                    [
+                        'message' => 'Invalid request',
+                    ]
+                );
+            } 
+            
+            if (isset($request->query->all()['per_page'])) {
+                $per_page = $request->query->all()['per_page'];
+            } else {
+                $per_page = 5;
+            }
+            
+
+            if (isset($request->query->all()['lang'])) {
+                $langCode = $request->query->all()['lang'];
+                $lang = $lr->findOneBy(
+                    [
+                        'code' => $langCode,
+                    ]
+                );
+                if (empty($lang)) {
+                    return new JsonResponse(
+                        [
+                            'message' => 'Language is not supported',
+                        ]
+                    );
+                }
+                $langId = $lang->getId();
+            } else {
                 $langCode = 'en';
                 $lang = $lr->findOneBy(
                     [
@@ -53,25 +73,12 @@ class HomeController extends AbstractController
                     ]
                 );
                 $langId = $lang->getId();
-            } else {
-                $langId = $lang->getId();
             }
-            
 
-        } else {
-            $langCode = 'en';
-            $lang = $lr->findOneBy(
-                [
-                    'code' => $langCode,
-                ]
-            );
-            $langId = $lang->getId();
+            $meals = $this->getMeals($request, $mr, $langId);
+
         }
 
-        if (isset($request->query->all()['tags'])) {
-            $tags = explode(',', $request->query->all()['tags']);
-            $meals = $mr->mealsByTag($langId, $tags);
-        }
         /**
          * fliters meals by category
          */
@@ -96,7 +103,7 @@ class HomeController extends AbstractController
                         ];
                     }
                 }
-            //all meals with category not null
+                //all meals with category not null
             } elseif ($request->query->all()['category'] == '!null') {
                 $meals = $mr->mealsWithCategoyNotNull($langId);
                 $meals = $paginator->paginate(
@@ -116,14 +123,17 @@ class HomeController extends AbstractController
                         ];
                     }
                 }
-            } 
+            }
             //meals with given category
             else {
                 $category = $request->query->all()['category'];
                 $meals = $mr->mealsByCategory($langId, $category);
                 if (empty($meals)) {
-                    echo 'No meils with that category';
-                    exit();
+                    return new JsonResponse(
+                        [
+                            'message' => 'No meils with that category',
+                        ]
+                    );
                 } else {
                     $meals = $paginator->paginate(
                         $meals,
@@ -144,13 +154,14 @@ class HomeController extends AbstractController
                     }
                 }
             }
-        //returns meals with given tags(if multiple tags are send return meals that contain at least one tag)
+            //returns meals with given tags(if multiple tags are send return meals that contain at least one tag)
         } elseif (isset($request->query->all()['tags'])) {
             $tags = explode(',', $request->query->all()['tags']);
             $meals = $mr->mealsByTag($langId, $tags);
             if (empty($meals)) {
-                echo 'No meals with that tags';
-                exit();
+                return new JsonResponse([
+                    'message' => 'No meals with that tags',
+                ]);
             } else {
                 $meals = $paginator->paginate(
                     $meals,
@@ -170,7 +181,7 @@ class HomeController extends AbstractController
                     }
                 }
             }
-        } 
+        }
         //if no tags or category are sent returns all meals
         else {
             $meals = $mr->meals($langId);
@@ -191,7 +202,6 @@ class HomeController extends AbstractController
                     ];
                 }
             }
-            
 
         }
 
@@ -205,11 +215,14 @@ class HomeController extends AbstractController
                 $category = [];
             }
 
-            if (strpos($request->query->all()['with'], 'tag') !== false) {
+            if (strpos($request->query->all()['with'], 'tags') !== false) {
                 foreach ($meals as $meal) {
                     $mealId = $meal['id'];
+                    //dump($mealId);
                     $tags = $tmr->mealTags($mealId);
-                    $tag = $tr->tagsById($langId, $tags);
+                    //dump($tags);
+                    $tag[] = $tr->tagsById($langId, $tags);
+                    //dump($tag);
 
                 }
             } else {
@@ -249,14 +262,116 @@ class HomeController extends AbstractController
             'self' => $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
         ];
 
-        $response = new JsonResponse([
-            'meta' => $meta,
-            'data' => $data,
-            'links' => $links,
-        ]);
-        $response->setEncodingOptions($response->getEncodingOptions() | JSON_PRETTY_PRINT);
-        return $response;
+        return $this->returnMeals($meta, $data, $links);
 
     }
 
+    /**
+     * @param $meta
+     * @param $data
+     * @param $links
+     * @return JsonResponse
+     */
+    public function returnMeals($meta, $data, $links)
+    {
+        $response = new JsonResponse(
+            [
+                'meta' => $meta,
+                'data' => $data,
+                'links' => $links,
+            ]
+        );
+        $response->setEncodingOptions($response->getEncodingOptions() | JSON_PRETTY_PRINT);
+        return $response;
+    }
+
+    public function getMeals($request, $mr, $lang)
+    {
+        if (isset($request->query->all()['category']) && isset($request->query->all()['diff_time'])) {
+            $category = $request->query->all()['category'];
+            $diffTime = $request->query->all()['diff_time'];
+            $meals = $mr->mealsByCategoryAndDiffTime($lang, $category, $diffTime);
+            dump($meals);
+        } elseif (isset($request->query->all()['category']) && isset($request->query->all()['tags'])) {
+            $category = $request->query->all()['category'];
+            $tags = explode(',', $request->query->all()['tags']);
+            $meals = $mr->mealsByCategoryAndTags($lang,$category,$tags);
+            dump($meals);
+        } elseif (isset($request->query->all()['category'])) {
+            $category = $request->query->all()['category'];
+            $meals = $mr->mealsByCategory($lang, $category);
+            dump($meals);
+        }
+
+        if ((isset($request->query->all()['tags']) && isset($request->query->all()['diff_time']))) {
+            $tags = explode(',', $request->query->all()['tags']);
+            $diffTime = $request->query->all()['diff_time'];
+            $meals = $mr->mealsByTagAndDiffTime($lang,$tags,$diffTime);
+            dump($meals);
+        }
+    }
+
+    public function validate($request)
+    {
+
+        if (isset($request->query->all()['per_page'])) {
+            if (!ctype_digit($request->query->all()['per_page'])) {
+                $valid = false;
+            } else {
+                $valid = true;
+            }
+        }
+
+        if (isset($request->query->all()['category'])) {
+            $category = $request->query->all()['category'];
+            if (ctype_digit($category) || $category === 'null' || $category === '!null') {
+                $valid = true;
+            } else {
+                $valid = false;
+            }
+        }
+
+        if (isset($request->query->all()['tags'])) {
+            $tags = explode(',', $request->query->all()['tags']);
+            foreach ($tags as $tag) {
+                if (ctype_digit($tag)) {
+                    $valid = true;
+                } else {
+                    $valid = false;
+                }
+            }
+        }
+
+        if (isset($request->query->all()['lang'])) {
+            if (is_string($request->query->all()['lang']) && !ctype_digit($request->query->all()['lang']) &&
+                strlen($request->query->all()['lang']) == 2) {
+                $valid = true;
+            } else {
+                $valid = false;
+            }
+        }
+
+        if (isset($request->query->all()['diff_time'])) {
+            if (!ctype_digit($request->query->all()['diff_time']) && $request->query->all()['diff_time'] <= 0) {
+                $valid= false;
+            } else {
+                $valid = true;
+            }
+        }
+
+        if (isset($request->query->all()['page'])) {
+            if (!ctype_digit($request->query->all()['page'])) {
+                $valid = false;
+            } else {
+                $valid = true;
+            }
+        }
+
+        if ($valid) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
